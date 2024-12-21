cc623ecc6dd866fdeb9b2bff7fbe52e9a11ff56a
İlhan Özen <ilhanozen.1504@gmail.com>
2024-12-21 22:49:15 +0300

    Visualized the median lines and strips. Added a green line that shows the closest pair at each step.

--- Source.cpp
+++ Source.cpp
@@ -36,14 +36,46 @@ Result naive(const vector<Point>& vec) {
 	return closestPair;
 }
 
-Result dacMerge(const vector<Point>& vec, int lo, int median, int hi, Result left, Result right) {
+Result dacMerge(const vector<Point>& vec, int lo, int median, int hi, Result left, Result right, sf::RenderWindow& window, double scale, const vector<sf::Vector2f>& animPoints, sf::CircleShape& point) {
 	Result closestPair;
-	if (left.distance < right.distance) {
+	if (left.distance <= right.distance) {
 		closestPair = left;
 	}
 	else {
 		closestPair = right;
 	}
+
+	window.clear(sf::Color::Color(0, 8, 16, 255));
+	for (const auto& p : animPoints) {
+		point.setPosition(p);
+		window.draw(point);
+	}
+
+	sf::Vertex closestLine[2];
+	closestLine[0].position = sf::Vector2f(400 + (closestPair.p1.x) * scale, 400 - (closestPair.p1.y) * scale);
+	closestLine[0].color = sf::Color::Color(115, 255, 130, 255);
+	closestLine[1].position = sf::Vector2f(400 + (closestPair.p2.x) * scale, 400 - (closestPair.p2.y) * scale);
+	closestLine[1].color = sf::Color::Color(115, 255, 130, 255);
+	window.draw(closestLine, 2, sf::Lines);
+
+	sf::Vertex line[2];
+	line[0].position = sf::Vector2f(animPoints[median].x, 25);
+	line[0].color = sf::Color::Red;
+	line[1].position = sf::Vector2f(animPoints[median].x, 775);
+	line[1].color = sf::Color::Red;
+	window.draw(line, 2, sf::Lines);
+
+	cout << closestPair.distance << endl;
+	int stripLen = closestPair.distance * 2 * scale;
+	cout << stripLen << endl;
+	int stripPosX = 400 + (vec[median].x * scale);
+	sf::RectangleShape rectangle(sf::Vector2f(stripLen, 750));
+	rectangle.setPosition(stripPosX - (stripLen / 2), 25);
+	rectangle.setFillColor(sf::Color(255, 240, 100, 32));
+	window.draw(rectangle);
+	window.display();
+	sf::sleep(sf::milliseconds(200));
+
 
 	vector<Point> strip;
 	double medianX = vec[median].x;
@@ -68,10 +100,27 @@ Result dacMerge(const vector<Point>& vec, int lo, int median, int hi, Result lef
 		}
 	}
 
+	window.clear(sf::Color::Color(0, 8, 16, 255));
+	for (const auto& p : animPoints) {
+		point.setPosition(p);
+		window.draw(point);
+	}
+
+	closestLine[0].position = sf::Vector2f(400 + (closestPair.p1.x) * scale, 400 - (closestPair.p1.y) * scale);
+	closestLine[1].position = sf::Vector2f(400 + (closestPair.p2.x) * scale, 400 - (closestPair.p2.y) * scale);
+	window.draw(closestLine, 2, sf::Lines);
+	
+	window.draw(line, 2, sf::Lines);
+
+	window.draw(rectangle);
+
+	window.display();
+	sf::sleep(sf::milliseconds(300));
+	
 	return closestPair;
 }
 
-Result dacRecursive(const vector<Point>& vec, int lo, int hi) {
+Result dacRecursive(const vector<Point>& vec, int lo, int hi, sf::RenderWindow& window, const vector<sf::Vector2f>& animPoints, sf::CircleShape& point, double scale) {
 	Result closestPair;
 	if (hi - lo == 1) {
 		closestPair.p1 = vec[lo];
@@ -86,17 +135,32 @@ Result dacRecursive(const vector<Point>& vec, int lo, int hi) {
 		return closestPair;
 	}
 	int median = (lo + hi) / 2;
-	Result left = dacRecursive(vec, lo, median);
-	Result right = dacRecursive(vec, median + 1, hi);
-	return dacMerge(vec, lo, median, hi, left, right);
+	window.clear(sf::Color::Color(0, 8, 16, 255));
+	for (const auto& p : animPoints) {
+		point.setPosition(p);
+		window.draw(point);
+	}
+
+	sf::Vertex line[2];
+	line[0].position = sf::Vector2f(animPoints[median].x, 25);
+	line[0].color = sf::Color::Red;
+	line[1].position = sf::Vector2f(animPoints[median].x, 775);
+	line[1].color = sf::Color::Red;
+	window.draw(line, 2, sf::Lines);
+	window.display();
+	sf::sleep(sf::milliseconds(200));
+
+	Result left = dacRecursive(vec, lo, median, window, animPoints, point, scale);
+	Result right = dacRecursive(vec, median + 1, hi, window, animPoints, point, scale);
+	return dacMerge(vec, lo, median, hi, left, right, window, scale, animPoints, point);
 }
 
-Result divideAndConquer(const vector<Point>& unsortedVec) {
+Result divideAndConquer(const vector<Point>& unsortedVec, sf::RenderWindow& window, const vector<sf::Vector2f>& animPoints, sf::CircleShape& point, double scale) {
 	vector<Point> vec = unsortedVec;
 	sort(vec.begin(), vec.end(), [](const Point& a, const Point& b) {
 		return a.x < b.x;
 		});
-	return dacRecursive(vec, 0, (vec.size() - 1));
+	return dacRecursive(vec, 0, (vec.size() - 1), window, animPoints, point, scale);
 }
 
 int main() {
@@ -104,6 +168,10 @@ int main() {
 	double x, y;
 	vector<Point> points;
 	ifstream pointFile("points_.txt");
+
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Closest Pairs");
+	sf::CircleShape point(1);
+	point.setFillColor(sf::Color::Color(255, 199, 0));
 
 	if (!pointFile.is_open()) {
 		cout << "Unable to open file." << endl;
@@ -118,16 +186,6 @@ int main() {
 		}
 	}
 
-	Result result = divideAndConquer(points);
-
-	cout << "Closest Pair: (" << result.p1.x << ", " << result.p1.y << ") and ("
-		<< result.p2.x << ", " << result.p2.y << ")" << endl;
-	cout << "Distance: " << result.distance << endl;
-
-	sf::RenderWindow window(sf::VideoMode(800, 800), "Closest Pairs");
-	sf::CircleShape point(1);
-	point.setFillColor(sf::Color::Color(255, 199, 0));
-	
 	double minX = DBL_MAX, minY = DBL_MAX, maxX = -DBL_MAX, maxY = -DBL_MAX;
 	for (const auto& p : points) {
 		if (p.x < minX) minX = p.x;
@@ -147,6 +205,16 @@ int main() {
 		animPoints.emplace_back(windowCenter.x + scaledX, windowCenter.y - scaledY);
 	}
 
+	sort(animPoints.begin(), animPoints.end(), [](const sf::Vector2f& a, const sf::Vector2f& b) {
+		return a.x < b.x;
+		});
+
+	Result result = divideAndConquer(points, window, animPoints, point, scale);
+	cout << "Closest Pair: (" << result.p1.x << ", " << result.p1.y << ") and ("
+		<< result.p2.x << ", " << result.p2.y << ")" << endl;
+	cout << "Distance: " << result.distance << endl;
+
+
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -154,12 +222,6 @@ int main() {
 				window.close();
 		}
 
-		window.clear(sf::Color::Color(0, 8, 16, 255));
-		for (const auto& p : animPoints) {
-			point.setPosition(p);
-			window.draw(point);
-		}
-		window.display();
 	}
 
 	return 0;
